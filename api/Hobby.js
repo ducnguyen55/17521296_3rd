a03c464d4ef0cb1d81f05c86eaf298454414cbf7
nguyenthanhduc4444 <nguyenthanhduc4444@gmail.com>
2020-03-29 00:12:39 +0700

    add

--- api/Hobby.js
+++ api/Hobby.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/:id',(req,res,next)=>{
-    res.status(200).json({
-        message : "YOUR ID ENTERED" + req.params.id
+    res.status(405).json({
+        Error: 'Method not allowed'
     });
 });
 module.exports=router;
