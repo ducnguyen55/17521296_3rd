dabe64834232467430fac34c08602655a46ded01
nguyenthanhduc4444 <nguyenthanhduc4444@gmail.com>
2020-03-29 00:03:24 +0700

    add

--- api/Hobby.js
+++ api/Hobby.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/:id', (req, res, next) => {
-       res.send('YOU ENTERD ID IS' + req.params.id);
+router.post('/:id',(req,res,next)=>{
+    res.send("Your'id entered" + req.params.id);
+    res.send(req.query);
 });
 module.exports=router;
