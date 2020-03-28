421239f6873d54667ef57b9f749094bc62986b08
nguyenthanhduc4444 <nguyenthanhduc4444@gmail.com>
2020-03-28 20:28:09 +0700

    add

--- app.js
+++ app.js
@@ -2,6 +2,27 @@ const express = require('express');
 const app= express();
 const bodyParser = require('body-parser');
 
+const swaggerJsDoc=require('swagger-jsdoc');
+const swaggerUi=require('swagger-ui-express');
+
+//Extended: https://swagger.io/specification/#infoObject
+const swaggerOptions={
+    swaggerDefinition: {
+        info: {
+            title: 'Restful API',
+            description: "API information",
+            contact: {
+                name: "Nguyễn Thanh Đức"
+            },
+            servers: ["http://localhost:3000"]
+        }
+    },
+    apis: ["app.js"]
+};
+
+const swaggerDocs=swaggerJsDoc(swaggerOptions);
+app.use('/apidocs',swaggerUi.serve,swaggerUi.setup(swaggerDocs));
+
 const ProfileRouter=require('./api/Profile');
 const HobbyRouter=require('./api/Hobby');
 app.use('/profile',ProfileRouter);
